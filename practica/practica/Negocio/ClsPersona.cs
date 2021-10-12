c47a1c19cac55f963d70f8616b492c782292a6b7
AlexSanz02 <zs0128032021@unab.edu.sv>
2021-10-12 08:55:39 -0600

    if

--- practica/practica/Negocio/ClsPersona.cs
+++ practica/practica/Negocio/ClsPersona.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace practica.Negocio
 {
@@ -14,6 +15,20 @@ namespace practica.Negocio
         {
             create(per);
 
+        }
+
+        public void Restriccion()
+        {
+            int edadPersona = 0;
+            if (edadPersona >= 18)
+            {
+                MessageBox.Show("Registro completado satisfactoriamente");
+            }
+            else
+                if (edadPersona < 18)
+            {
+                MessageBox.Show("Registro no completado, necesita ser mayor de edad");
+            }
         }
 
         public List<persona> ListFinal()
